0ac205b92d2a84b9832c764f21bab09bc2a8cda8
narayanprusty <arsenal.narayan@gmail.com>
2023-12-07 19:19:35 +0530

    fix: added seconds per year

--- contracts/constants.sol
+++ contracts/constants.sol
@@ -6,3 +6,6 @@ uint256 constant EXP_SCALE = 1e18;
 
 /// @dev A unit (literal one) in EXP_SCALE, usually used in additions/subtractions
 uint256 constant MANTISSA_ONE = EXP_SCALE;
+
+/// @dev The approximate number of seconds per year
+uint256 constant SECONDS_PER_YEAR = 31_536_000;
