29db1cdc8455a04736fcc2fc5e5cf6221aa5050d
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-12-19 19:01:20 +0530

    feat: add TimeManagerV5 and TimeManagerV8

---
+++ tests/hardhat/TimeManagerV5.ts
@@ -0,0 +1,34 @@
+import chai from "chai";
+import { ethers } from "hardhat";
+
+const { expect } = chai;
+
+describe("TimeManagerV5: tests", async () => {
+  let timeManager: ethers.Contracts;
+  describe("For block number", async () => {
+    const blocksPerYear = 10512000;
+    beforeEach(async () => {
+      const timeManagerV5 = await ethers.getContractFactory("TimeManagerV5");
+      timeManager = await timeManagerV5.deploy(false, blocksPerYear);
+    });
+
+    it("Retrieves block timestamp", async () => {
+      const currentBlockNumber = (await ethers.provider.getBlock("latest")).number;
+      expect(await timeManager.getBlockNumberOrTimestamp()).to.be.equal(currentBlockNumber);
+      expect(await timeManager.blocksOrSecondsPerYear()).to.be.equal(blocksPerYear);
+    });
+  });
+  describe("For block timestamp", async () => {
+    beforeEach(async () => {
+      const timeManagerV5 = await ethers.getContractFactory("TimeManagerV5");
+      timeManager = await timeManagerV5.deploy(true, 0);
+    });
+
+    it("Retrieves block timestamp", async () => {
+      const secondsPerYear = await timeManager.SECONDS_PER_YEAR();
+      const currentBlocktimestamp = (await ethers.provider.getBlock("latest")).timestamp;
+      expect(await timeManager.getBlockNumberOrTimestamp()).to.be.equal(currentBlocktimestamp);
+      expect(await timeManager.blocksOrSecondsPerYear()).to.be.equal(secondsPerYear);
+    });
+  });
+});
