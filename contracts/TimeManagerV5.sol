e3f9df8370b72d3f0631b2547911a96d4451385c
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-12-21 11:08:44 +0530

    refactor: add gap

--- contracts/TimeManagerV5.sol
+++ contracts/TimeManagerV5.sol
@@ -12,6 +12,13 @@ contract TimeManagerV5 {
     bool public isTimeBased;
 
     /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[47] private __gap;
+
+    /**
      * @dev Retrieves the current slot
      * @return Current slot
      */
--- contracts/TimeManagerV8.sol
+++ contracts/TimeManagerV8.sol
@@ -10,6 +10,13 @@ abstract contract TimeManagerV8 {
     /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
     bool public immutable isTimeBased;
 
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[48] private __gap;
+
     /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
     function() view returns (uint256) private immutable _getCurrentSlot;
 
