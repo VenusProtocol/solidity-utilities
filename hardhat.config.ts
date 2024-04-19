6c2d81e76e7888f4f68719efefc4254585f4e757
Kirill Kuvshinov <kkirka@gmail.com>
2024-04-19 11:22:00 +0300

    chore: specify paris hardfork explicitly
    Although hardhat uses paris by default for solc >=0.8.20, this
    might not be the case in future versions of hardhat. It's better
    to specify the hardfork explicitly, and update it when the chains
    are ready.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -38,6 +38,7 @@ const config: HardhatUserConfig = {
             enabled: true,
             runs: 200,
           },
+          evmVersion: "paris",
           outputSelection: {
             "*": {
               "*": ["storageLayout"],
