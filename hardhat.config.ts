d7ebaf79b52b586de82129c4efde630acd1adc94
Debugger022 <debugger022@gmail.com>
2024-11-15 23:15:33 +0530

    chore: update hardhat config and dependencies to support cancun VM version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -38,7 +38,7 @@ const config: HardhatUserConfig = {
             enabled: true,
             runs: 200,
           },
-          evmVersion: "paris",
+          evmVersion: "cancun",
           outputSelection: {
             "*": {
               "*": ["storageLayout"],
